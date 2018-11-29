b07efedb4b05b02979c5cb296ee10ca2efeee5ba
Brett Neese <brett@neese.rocks>
2018-11-28 20:43:56 -0800

    add some basic creation

--- api/database/config/index.js
+++ api/database/config/index.js
@@ -1,19 +1,45 @@
 "use strict";
-var log = console;
-
-var router = require("express").Router();
+const log = console;
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+const router = require("express").Router();
 
 router.use(function(req, res, next) {
   log.info("database.config router taking control");
   next();
 });
 
-router.get("/config", function(req, res, next){
+router.get("/config", function(req, res, next) {
   res.send("hello config");
 });
 
-router.post("/config/:name", function(req, res, next){
-    res.send(req.params.name);
+router.get("/config/:name", function(req, res, next) {
+  let params = {
+    TableName: "tlauv-staging-tlauvTable-ZZDF5BP2PVKU",
+    Item: {
+      Path: "database/config",
+      Key: req.params.name,
+      Value: {
+        plugin_name: "mongodb-database-plugin",
+        allowed_roles: "readonly",
+        connection_url:
+          "mongodb://{{username}}:{{password}}@mongodb.acme.com:27017/admin?ssl=true",
+        write_concern: '{ "wmode": "majority", "wtimeout": 5000 }',
+        username: "admin",
+        password: "Password!"
+      }
+    }
+  };
+
+  docClient.put(params, function(err, data) {
+    if (err) {
+      log.error(err);
+      res.status(500);
+    } else {
+      log.info(data);
+      res.status(204);
+    }
+  });
 });
 
 module.exports = router;
